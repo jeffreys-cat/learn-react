6da859ee525a98d7a9eed5a2042bfb6cb6fa6ede
HuJian <624598206@qq.com>
2018-02-05 16:20:09 +0800

    feat(redux-logger): add redux-logger

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import rootReducers from './reducers/index.reducer';
 
 import routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 import './index.scss';
 
+const logger = createLogger();
+
 const store = createStore(
     rootReducers,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
