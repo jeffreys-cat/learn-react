9404db2f4d3fd9c2fba737239deb9120bf14f565
HuJian <624598206@qq.com>
2018-02-05 14:08:44 +0800

    refactor(home): remove unused code

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import './home.scss';
-// import swal from 'sweetalert2';
 import { IHomeProps, IHomeState } from './home.model';
 import { connect } from 'react-redux';
 import { mapDispatchToProps } from '../../maps/mapDispatchToProps';
@@ -13,28 +12,9 @@ class Home extends React.Component<IHomeProps, IHomeState> {
             enthusiasmLevel: 1
         };
     }
-    getExclamationMarks(num: number) {
-        return Array(num + 1).join('!');
-    }
-    // onIncrement() {
-    //     let enthusiasmLevel = this.state.enthusiasmLevel;
-    //     enthusiasmLevel++;
-    //     this.setState({
-    //         enthusiasmLevel: enthusiasmLevel
-    //     });
-    // }
-    // onDecrement() {
-    //     if (this.state.enthusiasmLevel <= 0) {
-    //         swal('Oops...', 'enthusiasmLevel不能小于0', 'error');
-    //         return;
-    //     }
-    //     let enthusiasmLevel = this.state.enthusiasmLevel;
-    //     enthusiasmLevel--;
-    //     this.setState({
-    //         enthusiasmLevel: enthusiasmLevel
-    //     });
-    // }
+    // render
     render() {
+        const { onDecrement, onIncrement } = this.props;
         return (
             <div className="hello">
                 <div className="greeting">
@@ -42,8 +22,8 @@ class Home extends React.Component<IHomeProps, IHomeState> {
                      {/* Hello {name + this.getExclamationMarks(this.state.enthusiasmLevel)} */}
                 </div>
                 <div className="button-group">
-                    <button className="button-material" onClick={this.props.onDecrement}>-</button>
-                    <button className="button-material" onClick={this.props.onIncrement}>+</button>
+                    <button className="button-material" onClick={onDecrement}>-</button>
+                    <button className="button-material" onClick={onIncrement}>+</button>
                 </div>
                 <button className="button-material">Hello</button>
             </div>
