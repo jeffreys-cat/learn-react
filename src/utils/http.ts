753c23abe6752c8bd1931b3e6505e08abf3225e0
HuJian <624598206@qq.com>
2018-02-23 10:55:04 +0800

    refactor(http): update httpclient

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,15 +1,30 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { Observable } from 'rxjs';
 import { IResult } from '../models/http.model';
 
+/**
+ * HttpClient
+ */
 class HttpClient {
-    get(url: string, config?: AxiosRequestConfig): Observable<AxiosResponse<IResult<any>>> {
-        const observable = Observable.fromPromise(axios.get(url, config));
-        return observable;
+    /**
+     * @param url 
+     * @param config 
+     * @return Observable<IResult<any>>
+     */
+    get(url: string, config?: AxiosRequestConfig): Observable<IResult<any>> {
+        return Observable.fromPromise(axios.get(url, config))
+                    .map(response => response.data);
     }
-
-    post(url: string, data?: any, config?: AxiosRequestConfig) {
-        return axios.post(url, data, config);
+    /**
+     * @param url 
+     * @param data 
+     * @param config 
+     * @return Observable<IResult<any>>
+     */
+    post(url: string, data?: any, config?: AxiosRequestConfig): Observable<IResult<any>> {
+        return Observable.fromPromise(axios.post(url, data, config))
+                    .map(response => response.data);
     }
 }
+// Export A HttpClient Instance
 export const httpClient = new HttpClient();
